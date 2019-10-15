70d38c7e3973a18b1d1cb78dd452290814379801
Brian Taylor Johnson <brian.johnson@cigna.com>
2019-10-14 23:59:44 -0400

    Initial folder structure and Heroku deploy

---
+++ client/build/precache-manifest.ddc95d012e67e3c5d59bd2a292a8e5ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7057def700224f8668f88fb32209e564",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d50dc428edd2451da39",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c58f7d0850649a492c9b",
+    "url": "/static/js/2.0a3d259f.chunk.js"
+  },
+  {
+    "revision": "6d50dc428edd2451da39",
+    "url": "/static/js/main.41635aa0.chunk.js"
+  },
+  {
+    "revision": "9cdd1b2df75027a3eb2d",
+    "url": "/static/js/runtime-main.bfd03508.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
